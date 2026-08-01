dc030676acd1e67e7e779c499c71dbd374f3053e
srikesav09 <srikesav77@gmail.com>
2026-08-01 18:11:24 +0530

    feat(auth): add authentication service layer

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
-export const register = async (requestAnimationFrame,res) => {
-  res.status(200).json({
-    success: true,
-    message: "Register controller is working"
-  });
+import { registerUser } from "../services/auth.service.js";
+
+export const register = async (req, res) => {
+  const result = await registerUser(req.body);
+
+  res.status(200).json(result);
 };
---
+++ server/src/services/auth.service.js
@@ -0,0 +1,7 @@
+export const registerUser = async (userData) => {
+  return {
+    success: true,
+    message: "Register service is working",
+    data: userData,
+  };
+};
